7234fb68ecaeaec27968b03f819b5d1ebb107352
Juan <jgarcia1545@gmail.com>
2022-06-22 17:26:13 -0700

    Refactored the App.js File and began adding components

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+function Navigation(props) {
+    return (
+        <header>
+            <nav>
+                <ul>
+
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navigation;
